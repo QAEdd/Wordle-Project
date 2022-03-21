a065ec458b5c4614ef1433a6e03056138c692fee
QAEdd <edd-alexander@hotmail.com>
2022-03-21 14:56:18 +0000

    added jenkinsfile

--- application/__init__.py
+++ application/__init__.py
@@ -6,9 +6,9 @@ import pymysql
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = getenv('secret_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@<ip address>:3306/flash_demo'
-
+app.config['SECRET_KEY'] = getenv('secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@34.142.14.54:3306/wordle'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 import application.routes
